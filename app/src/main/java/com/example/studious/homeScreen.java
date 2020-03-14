96635172b5e686acd75ff86821bc9c474e4ff8e8
chower98 <56125443+chower98@users.noreply.github.com>
2020-03-14 12:05:45 -0500

    Update 2: Added Homescreen class, it has an error

---
+++ app/src/main/java/com/example/studious/homeScreen.java
@@ -0,0 +1,14 @@
+package com.example.studious;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class homeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_classes);
+    }
+}
