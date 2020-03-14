41c255294e16e1895dc6dc7b4d9e5f2469fb757f
chower98 <56125443+chower98@users.noreply.github.com>
2020-03-14 12:32:29 -0500

    Update 2: Added Homescreen class

--- app/src/main/java/com/example/studious/homeScreen.java
+++ app/src/main/java/com/example/studious/homeScreen.java
@@ -9,6 +9,6 @@ public class homeScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_classes);
+        setContentView(R.layout.activity_homeScreen);
     }
 }
