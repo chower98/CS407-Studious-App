b2034ea6ba8d98ab6af6b71544a8293a16b34417
Gene Lee <leexgenex@gmail.com>
2020-03-18 15:32:32 -0500

    added menu to home screen to log out

--- app/src/main/java/com/example/studious/HomeScreen.java
+++ app/src/main/java/com/example/studious/HomeScreen.java
@@ -1,8 +1,16 @@
 package com.example.studious;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 
 public class HomeScreen extends AppCompatActivity {
@@ -12,4 +20,38 @@ public class HomeScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
     }
+
+    public void coursesClick(View view) {
+        Intent coursesIntent = new Intent(this, AddClasses.class);
+        startActivity(coursesIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                // remove data kept in the instance for the user since they are logging out
+                SharedPreferences sharedPreferences = getSharedPreferences("com.example.studious", Context.MODE_PRIVATE);
+                sharedPreferences.edit().remove("username").apply();
+
+                Intent logoutIntent = new Intent(this, Login.class);
+                startActivity(logoutIntent);
+                return true;
+
+            case R.id.preferences:
+                Intent preferencesIntent = new Intent(this, Preferences.class);
+                startActivity(preferencesIntent);
+                return true;
+
+            default: return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/studious/Preferences.java
@@ -0,0 +1,6 @@
+package com.example.studious;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Preferences extends AppCompatActivity {
+}
