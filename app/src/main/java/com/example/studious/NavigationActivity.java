0510c98da78587979205d4ff5f9924d7ab86cdf0
Manya Waldia <waldia@wisc.edu>
2020-04-07 10:49:35 -0500

    It is a mess

---
+++ app/src/main/java/com/example/studious/NavigationActivity.java
@@ -0,0 +1,53 @@
+package com.example.studious;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+//import android.support.design.widget.BottomNavigationView;
+//import android.support.v7.app.AppCompatActivity;
+
+public class NavigationActivity extends AppCompatActivity {
+   private BottomNavigationItemView bottomNavigation;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.navigation);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+    }
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    BottomNavigationView.OnNavigationItemReselectedListener navigationItemReselectedListener = new BottomNavigationView.OnNavigationItemReselectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_matches:
+                    openFragment(MatchesFragment.newInstance("", ""));
+                    return true;
+                case R.id.navigation_recommendations:
+                    openFragment(RecommendationsFragment.newInstance("", ""));
+                    return true;
+                case R.id.navigation_requests:
+                    openFragment(RequestFragment.newInstance("", ""));
+                    return true;
+            }
+            return false;
+
+        }
+    };
+
+}
---
+++ app/src/main/java/com/example/studious/RecommendationsFragment.java
@@ -0,0 +1,9 @@
+package com.example.studious;
+
+import androidx.fragment.app.Fragment;
+
+public class RecommendationsFragment {
+    public static Fragment newInstance(String s, String s1) {
+        return null;
+    }
+}
