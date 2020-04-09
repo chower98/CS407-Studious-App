83f9eb92e725f5bbe481a9db808793156ece6f2f
Manya Waldia <waldia@wisc.edu>
2020-04-08 19:47:43 -0500

    New Message

--- app/src/main/java/com/example/studious/ui/Requests/DashboardFragment.java
+++ app/src/main/java/com/example/studious/ui/Requests/DashboardFragment.java
@@ -1,4 +1,4 @@
-package com.example.studious.ui.dashboard;
+package com.example.studious.ui.Requests;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
--- app/src/main/java/com/example/studious/ui/Requests/DashboardViewModel.java
+++ app/src/main/java/com/example/studious/ui/Requests/DashboardViewModel.java
@@ -1,4 +1,4 @@
-package com.example.studious.ui.dashboard;
+package com.example.studious.ui.Requests;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
